3da4eb2837f6039de56ecaa79711b8f4d0efe3e1
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-06-13 18:23:53 -0500

    FruitApp1.1.5

--- 06-Code/FruitApp/src/ec/edu/espe/FruitApp/view/FruitApp.java
+++ 06-Code/FruitApp/src/ec/edu/espe/FruitApp/view/FruitApp.java
@@ -1,14 +1,18 @@
 package ec.edu.espe.FruitApp.view;
 import com.mongodb.client.MongoCollection;
 import ec.edu.espe.FruitApp.model.Client;
-import ec.edu.espe.FruitApp.model.ConexionDB;
+
 import ec.edu.espe.FruitApp.model.Fruit;
 import ec.edu.espe.FruitApp.model.Register;
 import ec.edu.espe.FruitApp.model.Vegetable;
 import ec.edu.espe.FruitApp.model.Worker;
+import java.io.BufferedReader;
 import java.util.Scanner;
 import javax.swing.table.DefaultTableModel;
 import org.bson.Document;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 /**
  *
  *@author Carlos Granda,Ronny Ibarra,Jose Imbaquinga, DCCO-ESPE, Syntax Error
@@ -24,16 +28,15 @@ public class FruitApp {
         String name,email,color,colorVegetable,nameVegetable,
                 nameRegister,codeRegister,nameFruit;
        
-       
-        
-        
+        File fileC = new File("./NewClient.csv");
+        File fileW = new File("./Worker.csv");
+        File fileF = new File("./Fruit.csv");
+        File fileV = new File("./Vegetable.csv");
+        File fileP = new File("./Payments.csv");
         Client c1=new Client();
         System.out.println("WELCOME TO THE 'DON MORALES' FRUIT STORE SYSTEM ");
         System.out.println("Enter an option");
         Scanner entrada=new Scanner(System.in);
-      
-      
-        
          System.out.println("Option 1: Register a new client:");
          System.out.println("Option 2: Register a worker:");
          System.out.println("Option 3: Register the sale of a fruit:");
@@ -44,111 +47,110 @@ public class FruitApp {
          opcion2=entrada.nextInt();
          switch(opcion2){
              case 1:
-                data.put("New Client","");
                 System.out.println("Enter the id of the "
                         + "person you want to register:");
                 id=entrada.nextInt();
-                data.put("Id:",+id);
                 System.out.println("Enter the age of the person you "
                         + "want to register:");
                 age=entrada.nextInt();
-                data.put("Age:",+age);
                 System.out.println("Enter the name you want to register:");
                 name=entrada.next();
-                data.put("Name",name);
                 Client c2=new Client(name,id,age);
-        
                 System.out.println("The data to be entered is:"+c2);
-                break;
-                
+                try (FileWriter fileWc = new FileWriter(fileC,true);
+                        FileWriter fileWp = new FileWriter(fileC,true);){
+                        fileWc.write(c2.toString()+"\n");
+                    }catch(Exception e){
+                        System.out.println("An error has occurred");
+                    }
+                  break;
+                    
             case 2:
-                data.put("Register a worker:","");
                 System.out.println("Enter the id of "
                         + "the person you want to register:");
                 id=entrada.nextInt();
-                data.put("Id:",+id);
                 System.out.println("Enter the age of the "
                         + "person you want to register:");
                 age=entrada.nextInt();
-                data.put("Age",+age);
                 System.out.println("Enter the name you want to register:");
                 name=entrada.next();
-                data.put("Name",name);
                 System.out.println("Enter the email:");
                 email=entrada.next();
-                data.put("E-mail:",email);
                 System.out.println("Enter the cellphone "
                         + "of the person you want to register:");
                 cellphone=entrada.nextInt();
-                data.put("Cellphone",+cellphone);
                 Worker W1=new Worker(name,id,age,cellphone,email);
                 System.out.println("The data to be entered is:"+W1);
-               
-                 break; 
+               try (FileWriter fileWc = new FileWriter(fileW,true);
+                        FileWriter fileWp = new FileWriter(fileW,true);){
+                        fileWc.write(W1.toString()+"\n");
+                    }catch(Exception e){
+                        System.out.println("An error has occurred");
+                    }
+                  break;
+                 
                  
             case 3:
-                data.put("Fruit","");
                 System.out.println("Enter fruit name");
                 nameFruit=entrada.next();
-                data.put("Name: ", nameFruit);
                 System.out.println("Enter the color of the fruit");
                 color=entrada.next();
-                data.put("Color:", color);
                 System.out.println("Enter the amount to sell");
                 amount=entrada.nextInt();
-                data.put("Amount:",+amount);
                 System.out.println("Enter the weight of the fruit");
                 weight=entrada.nextDouble();
-                data.put("Weight", weight);
                 System.out.println("Enter the cost of the fruit:");
                 cost=entrada.nextDouble();
-                data.put("Cost",+cost);
                 Fruit F1=new Fruit(nameFruit,color,amount,weight,cost);
                 System.out.println("The data to be entered is:"+F1);
-              
+                try (FileWriter fileWc = new FileWriter(fileF,true);
+                            FileWriter fileWp = new FileWriter(fileF,true);){
+                        fileWc.write(F1.toString()+"\n");
+                    }catch(Exception e){
+                        System.out.println("An error has occurred");
+                    }
                 break;
             case 4:
-                data.put("Vegetable", "");
                 System.out.println("Enter name of vegetable:");
                 nameVegetable=entrada.next();
-                data.put("Name:",nameVegetable);
                 System.out.println("Enter the color of the vegetable:");
                 colorVegetable=entrada.next();
-                data.put("Color:",colorVegetable);
                 System.out.println("Enter the amount to sell:");
                 amountVegetable=entrada.nextInt();
-                data.put("Amount",+amountVegetable);
                 System.out.println("Enter the weight of the vegetable:");
                 weightVegetable=entrada.nextDouble();
-                data.put("Weigh :",+weightVegetable);
                 System.out.println("Enter the cost of the vegetable:");
                 costVegetable=entrada.nextDouble();
-                data.put("Costo; ",+costVegetable);
                 Vegetable V1=new Vegetable(nameVegetable,
                         colorVegetable,amountVegetable,
                         weightVegetable,costVegetable);
                 System.out.println("The data of the vegetables that are"
                         + " going to enter:"+V1);
-           
+                try (FileWriter fileWc = new FileWriter(fileV,true);
+                        FileWriter fileWp = new FileWriter(fileV,true);){
+                        fileWc.write(V1.toString()+"\n");
+                    }catch(Exception e){
+                        System.out.println("An error has occurred");
+                    }
                 break;
             case 5: 
-                data.put("Payments:","");
-                System.out.println("Enter the name of the worker to be paid:");
+                System.out.println("Enter the frist last name of the worker to be paid:");
                 nameRegister=entrada.next();
-                data.put("Name :",nameRegister);
                 System.out.println("Enter worker code:");
                 codeRegister=entrada.next();
-                data.put("Code ",codeRegister);
                 System.out.println("Enter the amount to pay:");
                 amountRegister=entrada.nextInt();
-                data.put("Amount: ",amountRegister);
                 System.out.println("Enter the id:");
                 idRegister=entrada.nextInt();
-                data.put("Id:",+idRegister);
                 Register R1=new Register(nameRegister,codeRegister,
                          amountRegister,idRegister);
                 System.out.println("The people who will pay are:"+R1);
-             
+                try (FileWriter fileWc = new FileWriter(fileP,true);
+                        FileWriter fileWp = new FileWriter(fileP,true);){
+                        fileWc.write(R1.toString()+"\n");
+                    }catch(Exception e){
+                        System.out.println("An error has occurred");
+                    }
                 break;
             case 6:
                 break;
