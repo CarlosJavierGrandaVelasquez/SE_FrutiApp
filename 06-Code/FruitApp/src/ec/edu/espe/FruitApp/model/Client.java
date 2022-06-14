3158a3097fe991169a8390af211a323a71aee1f8
Ronny Joel Ibarra Gaona <96481164+RonnyIbarra@users.noreply.github.com>
2022-06-13 20:45:46 -0500

    Update Client.java

--- 06-Code/FruitApp/src/ec/edu/espe/FruitApp/model/Client.java
+++ 06-Code/FruitApp/src/ec/edu/espe/FruitApp/model/Client.java
@@ -5,47 +5,47 @@ package ec.edu.espe.FruitApp.model;
  * @author Carlos Granda,Ronny Ibarra,Jose Imbaquinga, DCCO-ESPE, Syntax Error
  */
 public class Client {
-    private String name;
-    private int id;
-    private int age;
+   private String name;
+   private int id;
+   private int age;
 
-    public Client() {
+   public Client() {
     }
 
-    public Client(String name, int id, int age) {
-        this.name = name;
-        this.id = id;
-        this.age = age;
+   public Client(String name, int id, int age) {
+       this.name = name;
+       this.id = id;
+       this.age = age;
     }
 
     @Override
-    public String toString() {
-        return "Client{" + "name=" + name + ", id=" + id + ", age=" + age + '}';
+   public String toString() {
+       return "Client{" + "name=" + name + ", id=" + id + ", age=" + age + '}';
     }
     
  
     
-    public String getName() {
-        return name;
+   public String getName() {
+       return name;
     }
 
-    public void setName(String name) {
+   public void setName(String name) {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
+   public int getId() {
+       return id;
     }
 
-    public void setId(int id) {
+   public void setId(int id) {
         this.id = id;
     }
 
-    public int getAge() {
+   public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+   public void setAge(int age) {
         this.age = age;
     }   
 }
