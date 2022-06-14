b519c3c041882ddd98339729e40fde8337f8da39
Ronny Joel Ibarra Gaona <96481164+RonnyIbarra@users.noreply.github.com>
2022-06-13 20:47:00 -0500

    Update Fruit.java

--- 06-Code/FruitApp/src/ec/edu/espe/FruitApp/model/Fruit.java
+++ 06-Code/FruitApp/src/ec/edu/espe/FruitApp/model/Fruit.java
@@ -5,65 +5,65 @@ package ec.edu.espe.FruitApp.model;
  * @author Carlos Granda,Ronny Ibarra,Jose Imbaquinga, DCCO-ESPE, Syntax Error
  */
 public class Fruit {
-    private String name;
-    private String color;
-    private int amount;
-    private double weight;
-    private double cost;
+   private String name;
+   private String color;
+   private int amount;
+   private double weight;
+   private double cost;
 
-    public Fruit() {
+   public Fruit() {
     }
 
-    public Fruit(String name, String color, int amount, double weight, double cost) {
-        this.name = name;
-        this.color = color;
-        this.amount = amount;
-        this.weight = weight;
-        this.cost = cost;
+   public Fruit(String name, String color, int amount, double weight, double cost) {
+       this.name = name;
+       this.color = color;
+       this.amount = amount;
+       this.weight = weight;
+       this.cost = cost;
     }
     
     @Override
-    public String toString() {
-        return "Fruit{" + "name=" + name + ", color=" + color + ", amount=" + amount + ", weight=" + weight + ", cost=" + cost + '}';
+   public String toString() {
+       return "Fruit{" + "name=" + name + ", color=" + color + ", amount=" + amount + ", weight=" + weight + ", cost=" + cost + '}';
     }
     
-    public String getName() {
-        return name;
+   public String getName() {
+       return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+   public void setName(String name) {
+       this.name = name;
     }
 
-    public String getColor() {
-        return color;
+   public String getColor() {
+       return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+   public void setColor(String color) {
+       this.color = color;
     }
 
-    public int getAmount() {
+   public int getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount) {
+   public void setAmount(int amount) {
         this.amount = amount;
     }
 
-    public double getWeight() {
+   public double getWeight() {
         return weight;
     }
 
-    public void setWeight(double weight) {
+   public void setWeight(double weight) {
         this.weight = weight;
     }
 
-    public double getCost() {
+   public double getCost() {
         return cost;
     }
 
-    public void setCost(float cost) {
+   public void setCost(float cost) {
         this.cost = cost;
     }
     
